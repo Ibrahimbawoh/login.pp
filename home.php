0e364dc02a24c89c3faf094ad688047463901777
Ibrahimbawoh <153259786+Ibrahimbawoh@users.noreply.github.com>
2023-12-18 14:20:34 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,218 @@
+
+<!DOCTYPE html>
+<html >
+<head>
+  
+  <link rel="stylesheet" href="home.css">
+    <title>premalink business LTD</title>
+    <header style="font-size: 40px" font-family="sans-serif"> Bawoh cypher threat command center  and Digital Security awarness
+    </header>
+   
+        <nav class="day">
+          
+            <ul class="many">
+                <h1>Home </h1>
+                <li>home</li>
+                <li><a href="selfassist.html">self assist</li>
+                <li> <a href="../contact.html">contact us</li>
+                <li>whatsapp us now at 08978675664</li>
+                
+            </ul>
+        </nav>
+      <title>permalink business Limited
+</title>
+</head>  
+<body>
+  <div class="col-xs-3">
+    <div id="panel4" class="panel panel primary"></div>
+
+  </div>
+  <div class="cointainer">
+<img src="big.png" alt="hero"/>
+<div class="inner">
+  
+  </div>
+    
+</body>
+
+
+
+<div class="cover">
+<div class="box">
+
+
+</div>
+
+<paragraph class=" bawoh">
+  <h2 
+  style="text-align: center;";>
+    
+  We Are here to help
+  </h2>
+ 
+  
+</paragraph>
+
+ 
+<table class="BAWOH" border="1" cell padding="100">
+
+
+
+  
+  <caption style="font-size: 50px">SERVICES</caption>
+  <thead >
+  <tr class="BAWOH">
+  <th  height="400px" width="1000px" style="font-size: 40px"> 
+  
+    <div class="card">
+      <div class="card-img">
+        <img src=
+"ibrahim.jpg" alt="User 3">
+      </div>
+
+
+
+<div class="card-info">
+       
+        <paragraph class="daystar"><h1 font size="70px">why us </h1>
+    
+        </h2>
+        
+        
+      </div>
+    </div>
+  
+  
+  
+  </th>
+  <th width="1000px"> We take pride in securing & ptotecting your Mobile App and websites in Nairobi-Kenya that sets up brands for digital growth at scale, while ensuring customers are getting the unique experience of security.
+
+  </th>
+  </tr>
+  </thead>
+  <tbody >
+  <tr class="BA">
+  <td  height="450px" width="300px"colspan="2">Body content 1 </td>
+ 
+  
+  </tr>
+  </tbody>
+
+  <tbody >
+    <tr class="IB">
+    <td height="450px" width="300px"colspan="2"> 
+      
+
+ <div class="card-info">
+       
+      >
+      </div>
+    </div>
+  
+    <div class="card">
+      <div class="card-img">
+        <img src=
+"ibrahim.jpg" alt="User 3">
+      </div>
+
+<div class="card-info">
+       
+        <paragraph class="daystar"><h1 font size="70px">why choose us </br>    Build and develop relationships across the </br>different internal IT teams and with various stakeholders </br>so that the business unit remains involved </br>and aware of internal developments..</h1>
+    
+        </h2>
+        
+        
+      </div>
+    </div>
+
+    
+    <div class="card">
+      <div class="card-img">
+        
+        <img src=
+"plan.jpg" alt="User 2">
+      </div>
+
+
+      
+      
+      
+
+
+      <div class="card-info">
+        <paragraph class="daystar"><h1 font size="70px">Our plan and goals </br>we are one of  kenya leading cloud, website, mobile security and computing service provider in kenya. we have grown to be the most profitable arm of the instituation of kenya   businesses  with customers  trust ..</h1>
+        
+      </div>
+    </div>
+    
+    <div class="card">
+      <div class="card-img">
+        <img src=
+"bawoh.jpg" alt="User 3">
+      </div>
+
+
+
+      <paragraph class="daystar"><h1 font size="70px">customers </br>we have over 100 customers and has manage to maintain relationship of 91 and has help about 2000 people about the prcation and  safety measures in securing themselves from any cypher attack we also provide  method to used in securing yourself online and keeping your social media accounts safe   .</h1>
+    
+    
+    </tr>
+    </tbody>
+  <tfoot>
+  <tr class="4">
+  
+  <td height="750px" width="10000PX"img src="lion.png" alt="hero" colspan="2">  Footer content </td>
+  
+ 
+  </tr>
+  </tfoot>
+  </table>
+  
+
+
+<footer class="footer" >
+  
+  <div class="cointainer">
+  <div class="row">
+    <div class="footer-coln">
+      <h4>about us</h4>
+      <ul>
+        <li>
+          Identifies risks for data theft and loss 
+
+          
+        </li>
+      </ul>
+  </div>
+
+<div>
+  <div class="footer-coln">
+    <h4>get in touch with now
+      .
+
+      .</h4>
+    <ul>
+      <li><a href="#"> contact</a></li>
+      <li><a href="#"> 075646466576</a></li>
+    </ul>
+</div>
+
+  </div>
+  </div>
+
+  <script >
+  
+  (function(message) {
+alert(message);
+}("sucessfull !"));
+
+
+
+
+ </script>
+ 
+</footer> 
+</html>
+
+
+  
